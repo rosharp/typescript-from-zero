ccbcff3ba45f4c4332f75d26dabe963a222c98e7
rosharp <ro.moreko@gmail.com>
2022-05-30 16:03:04 +1000

    015 Кортежи

--- typescript/app.js
+++ typescript/app.js
@@ -1,10 +1,2 @@
 "use strict";
-const skills = ['Dev', 'DevOps', 'Testing']; // Не просто массив, а массив строк
-for (let skill of skills) {
-    console.log(skill.toUpperCase());
-}
-const res = skills
-    .filter((s) => s !== 'DevOps')
-    .map(s => s + '! ')
-    .reduce((a, b) => a + b);
-console.log(res);
+const skill = [1, 'Dev'];
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,12 +1,13 @@
-const skills: string[] = ['Dev', 'DevOps', 'Testing']; // Не просто массив, а массив строк
+const skill: [number, string] = [1, 'Dev'];
+// const id = skill[0];
+// const skillName = skill[1];
+// // const q = skill[2]; - к нему мы обратиться не можем
 
-for (let skill of skills) {
-  console.log(skill.toUpperCase());
-}
+// skill.push('Testing');
+// // const third = skill[2]; - все равно не можем, кортеж уже типизирован
+// skill.pop();
 
-const res = skills
-  .filter((s: string) => s !== 'DevOps')
-  .map(s => s + '! ')
-  .reduce((a, b) => a + b);
+const [id, skillName] = skill; // деструктуризация, как в реакте - удобно вытаскивать элементы из кортежей
 
-console.log(res);
+const arr: [number, string, ...boolean[]] = [1, 'something', true, true, false]
+// Производное число boolean через spread operator
