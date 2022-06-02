981823f6498831c22c40c8daa579eebaca3a7cfd
rosharp <ro.moreko@gmail.com>
2022-06-03 08:36:59 +1000

    025 Optional

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,20 @@
 "use strict";
-// interface User {
-//   name: string,
-//   age: number,
-// };
+const user = {
+    login: 'a@a.ru',
+    password: '1'
+};
+function multiply(first, second) {
+    // мы могли сказать second?: number = 5
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+multiply(5);
+function testPass(user) {
+    var _a;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+}
+function test(param) {
+    const t = param !== null && param !== void 0 ? param : multiply(5); // ?? проверяет, если парамс нулл или андефайнд - умножить на 5, если да
+}
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,19 +1,37 @@
-// interface User {
-//   name: string,
-//   age: number,
-// };
+interface User {
+  login: string,
+  password?: string,
+  // ? добавляет опциональность
+  // неэквивалентна string | undefined - если удалить пароль в консте, то будет ошибка
+}
 
-// interface User {
-//   age: number,
-// } // Один интерфейс дополняет другой
+const user: User = {
+  login: 'a@a.ru',
+  password: '1'
+}
 
-// const user: User = {
-//   name: 'as',
-//   age: 33,
-// }
+function multiply(first: number, second?: number): number {
+  // мы могли сказать second?: number = 5
+  if (!second) {
+    return first * first;
+  } 
 
-type ID = string | number; // работает с примитивными типами
+  return first * second;
+}
 
-interface IDI { // нельзя заэкстендить от примитивных типов
-  ID: string | number; // должен работать с описаниями классов и сложными типами
+multiply(5);
+
+interface UserPro {
+  login: string,
+  password?: {
+    type: 'primary' | 'secondary'
+  },
+}
+
+function testPass(user: UserPro) {
+  const t = user.password?.type;
+}
+
+function test(param?: string) {
+  const t = param ?? multiply(5); // ?? проверяет, если парамс нулл или андефайнд - умножить на 5, если да
 }
