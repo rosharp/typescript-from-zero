e98644ecb716fa15e8b746537f4630cf93035636
rosharp <ro.moreko@gmail.com>
2022-06-05 10:39:25 +1000

    035 Создание класса

--- typescript/app.js
+++ typescript/app.js
@@ -1,38 +1,11 @@
 "use strict";
-// never - никогда не будет присвоено
-function generateError(message) {
-    throw new Error(message);
-}
-function dumpError() {
-    // return '' => error
-    while (true) { }
-}
-function rec() {
-    return rec();
-}
-const a = undefined;
-function processAction(action) {
-    switch (action) {
-        case 'refund':
-            // ...
-            break;
-        case 'checkout':
-            // ...
-            break;
-        case 'reject':
-            // ...
-            break;
-        default:
-            const _ = action;
-            throw new Error('Action not found');
+class User {
+    constructor(name) {
+        this.name = name;
     }
 }
-function isString(x) {
-    if (typeof x === 'string') {
-        return true;
-    }
-    else if (typeof x === 'number') {
-        return false;
-    }
-    generateError('Incorrect type'); // Исчерпывающая проверка
-}
+const user = new User('Vasya');
+console.log(user);
+user.name = 'Petya';
+console.log(user);
+
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,52 +1,23 @@
-// помогает в каждой проверке определнный тип, через который можно будет обращаться к нужным свойствам и к нужным объектам
-
-interface User {
+class User {
   name: string;
-  email: string;
-  login: string;
-}
 
-const user: User = {
-  name: 'Vasya',
-  email: 'vasily@gmail.com',
-  login: 'vasya'
-} as User;
-
-interface Admin {
-  name: string;
-  role: number;
-}
-
-const admin: Admin = {
-  ...user,
-  role: 1
-}
-
-function logId(id: string | number) {
-  if (isString(id)) {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
+  constructor(name: string) {
+    this.name = name;
   }
 }
 
-// type guard
-function isString(x: string | number): x is string {
-  return typeof x === 'string';
+const user = new User('Vasya');
+console.log(user);
+user.name = 'Petya';
+console.log(user);
+
+class Admin {
+    role: number;
+
+    // constructor(role: number) {
+    //   this.role = role;
+    // }
 }
 
-function isAdmin(user: User | Admin): user is Admin {
-  return 'role' in user;
-}
-
-function isAdminAlternative(user: User | Admin): user is Admin {
-  return (user as Admin).role !== undefined;
-}
-
-function setRoleZero(user: User | Admin) {
-  if (isAdmin(user)) {
-    user.role = 0;
-  } else {
-    throw new Error('The user is not Admin');
-  }
-}
+const admin = new Admin();
+admin.role = 1;
