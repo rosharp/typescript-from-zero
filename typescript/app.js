317873534859a9efbcfd82b95f87b16d5ddf2cfa
rosharp <ro.moreko@gmail.com>
2022-06-05 10:57:38 +1000

    036 Конструктор

--- typescript/app.js
+++ typescript/app.js
@@ -1,11 +1,18 @@
 "use strict";
+// Для 1-3 реализаций
 class User {
-    constructor(name) {
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === "string") {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === "number") {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
-const user = new User('Vasya');
-console.log(user);
-user.name = 'Petya';
-console.log(user);
-
+const user = new User("Vasya");
+const user2 = new User();
+const user3 = new User(33);
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,23 +1,27 @@
+// Для 1-3 реализаций
+
 class User {
   name: string;
+  age: number;
 
-  constructor(name: string) {
-    this.name = name;
+  constructor();
+  constructor(name: string);
+  constructor(age: number); // Конструктор реализации
+  constructor(name: string, age: number);
+
+  constructor(ageOrName?: string | number, age?: number) { // Конструктор имплементации
+    if (typeof ageOrName === "string") {
+      this.name = ageOrName;
+    } else if (typeof ageOrName === "number") {
+      this.age = ageOrName;
+    }
+
+    if (typeof age === 'number') {
+      this.age = age;
+    }
   }
 }
 
-const user = new User('Vasya');
-console.log(user);
-user.name = 'Petya';
-console.log(user);
-
-class Admin {
-    role: number;
-
-    // constructor(role: number) {
-    //   this.role = role;
-    // }
-}
-
-const admin = new Admin();
-admin.role = 1;
+const user = new User("Vasya");
+const user2 = new User();
+const user3 = new User(33);
