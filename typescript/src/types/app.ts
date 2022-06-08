6ec6ad945a96552624740a9aa08ee203af78c7a9
rosharp <ro.moreko@gmail.com>
2022-06-08 16:30:34 +1000

    059 Пример встроенных Generics

--- typescript/src/types/app.ts
+++ typescript/src/types/app.ts
@@ -1,34 +1,12 @@
-class User {
-	role: 'admin' | 'user';
-	constructor(public name: string) {
-		this.name = name;
-	}
+const num: Array<number> = [1, 2, 3];
 
-	login(): void;
+async function test() {
+	const a = await new Promise<number>((resolve, reject) => {
+		resolve(1);
+	});
 }
 
-	function createUser(user: User) { // noUnusedParameters будет предупреждать о неиспользованных параметрах
-		// logic
-
-		const defaultUser = new User('default'); // noUnusedLocals позволит избавиться от неиспользованных переменных
-		defaultUser.role = undefined; // exactOptionalPropertyTypes 
-
-		switch(user.role) {
-			case 'admin':
-				// noFallthroughCasesInSwitch => если нет ни ретурна, ни брейка, то провала не будет 
-			case 'user':
-				return true;
-				const c = 1; // allowUnreachableCode выключит предупреждения при недостижимом коде
-		}
-	}
-
-interface IChecks {
-	[check: string]: boolean;
-}
-
-const c: IChecks = { 'kpp': true };
-const d = c['drive']; // noUncheckedIndexedAccess => Add 'undefined' to a type when accessed using an index
-
-class VipUser extends User {
-	login(): void // noImplicitOverride => error
+const check: Record<string, boolean> = {
+	drive: true,
+	kpp: false	
 }
