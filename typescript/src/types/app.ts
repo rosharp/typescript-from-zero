37e475d00c50f01b06417d1c0c8a1539ca555873
rosharp <ro.moreko@gmail.com>
2022-06-14 23:52:22 +1000

    068 Keyof

--- typescript/src/types/app.ts
+++ typescript/src/types/app.ts
@@ -1,42 +1,18 @@
-// позволяет писать еще один вариант extends, обернутый в функцию
-// его преимущество: дополнительный тайп-чекинг и возможность
-// миксить свосйства сразу нескольких классов
+// встроенные дженерики
+// зачем нужны и как используются
+// зачем нужны обобщения с точки зрения типов
+// в жс нет необходимости в типизации, но когда она появляются
+// нужны обощения, которые повозволят переиспользовать код
 
-// хороши, когда необходимо взять элементы из двух разных областей
-// и совместить функционал
+const a: Array<number> = [1, 2, 3];
 
-type Contructor = new (...args: any) => {}
-type GContructor<T = {}> = new (...args: any) => T
-
-class List {
-	constructor(public items: string[]) {}
+async function test() {
+	const b = new Promise<number>((resolve, reject) => {
+		resolve(1);
+	})
 }
 
-class Accordion {
-	isOpened: boolean;
+const check: Record<string, boolean> = {
+	drive: true,
+	kpp: false
 }
-
-type ListType = GContructor<List>; 
-type AccordionType = GContructor<Accordion>; 
-
-class ExtendedListClass extends List {
-	first() {
-		return this.items[0];
-	}
-}
-
-function ExtendedList<TBase extends ListType & AccordionType>(Base: TBase) {
-	return class ExtendedList extends Base {
-		first() {
-			return this.items[0];
-		}
-	}
-}
-class AccordionList {
-	isOpened: boolean;
-	constructor(public items: string[]) {}
-}
-
-const list = ExtendedList(AccordionList);
-const res = new list(['first', 'second']);
-console.log(res.first());
